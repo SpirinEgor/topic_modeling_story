3563ef676002c6e829ae30379b9fd7d0c52b109e
Egor Spirin <spirin.egor@mail.ru>
2021-12-01 01:43:20 +0300

    Analyze topics in specified versions

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -3,25 +3,23 @@ from collections import Counter
 from dataclasses import asdict
 from os.path import join
 from random import randint
+from typing import List
 
-import plotly.express as px
 import pyLDAvis as lda_vis
 import streamlit as st
 import streamlit.components.v1 as components
-from plotly.graph_objs import Figure
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+from numpy import ndarray
+import seaborn as sns
+from pyLDAvis import PreparedData
 
 from src.dataset import YouTrackIssueDataset
 
 LDA_VIS_DATA = join("models", "lda_vis_data.pkl")
 DATA_PATH = join("data", "pycharm_issues.json")
+PREDICTION_DATA = join("models", "predictions.pkl")
 N_RANDOM_EXAMPLES = 10
-
-
-def build_lda_vis_html() -> str:
-    with open(LDA_VIS_DATA, "rb") as f:
-        lda_vis_data = pickle.load(f)
-    vis_pd = lda_vis.prepare(**lda_vis_data)
-    return lda_vis.prepared_data_to_html(vis_pd, template_type="simple")
 
 
 def version_counts(dataset: YouTrackIssueDataset) -> Figure:
@@ -29,12 +27,46 @@ def version_counts(dataset: YouTrackIssueDataset) -> Figure:
     all_counts = Counter(version).items()
     sorted_counts = sorted(all_counts, key=lambda x: int(x[0][:4] + x[0][5]))
     versions, counts = zip(*sorted_counts)
-    fig = px.bar(x=list(versions), y=list(counts))
+    fig = plt.figure(figsize=(20, 5))
+    sns.set_theme()
+    sns.barplot(x=list(versions), y=list(counts))
     return fig
+
+
+def retrieve_top_topics_from_version(
+    dataset: YouTrackIssueDataset, predictions: ndarray, version: str, top_k: int = 10
+) -> List:
+    version_indices = [i for i in range(len(dataset)) if version in dataset[i][1]]
+    topic = predictions[version_indices].argmax(-1)
+    counts = Counter(topic)
+    return counts.most_common(top_k)
+
+
+def get_top_topic_words(topic_id: int, vis_pd: PreparedData, top_k: int = 10) -> List[str]:
+    topic_mask = vis_pd.topic_info.Category == f"Topic{topic_id}"
+    return vis_pd.topic_info[topic_mask].sort_values(by="Freq", ascending=False).Term[:top_k].tolist()
+
+
+def show_version(version: str, dataset: YouTrackIssueDataset, prediction: ndarray, vis_pd: PreparedData):
+    top_topics = retrieve_top_topics_from_version(dataset, prediction, version)
+    top_topics_words = "\n".join(
+        [
+            f"Topic {topic_id + 1} ({topic_cnt}): " + ", ".join(get_top_topic_words(topic_id + 1, vis_pd))
+            for topic_id, topic_cnt in top_topics
+        ]
+    )
+    st.markdown(f"Top-10 most common topics for issues for `{version}` version")
+    st.text(top_topics_words)
 
 
 def main():
     dataset = YouTrackIssueDataset(DATA_PATH)
+    with open(PREDICTION_DATA, "rb") as f:
+        prediction = pickle.load(f)
+    with open(LDA_VIS_DATA, "rb") as f:
+        lda_vis_data = pickle.load(f)
+    vis_pd = lda_vis.prepare(**lda_vis_data)
+
     st.set_page_config(layout="wide")
     st.title("Topic modeling on YouTrack issues from PyCharm users.")
 
@@ -44,7 +76,7 @@ def main():
 
     This streamlit application explains suggested a solution.
     Full code, including model training and all weights, may be found on [GitHub](https://github.com/SpirinEgor/topic_modeling_story).
-    """.strip()
+    """
     )
 
     st.header("Data overview")
@@ -56,7 +88,7 @@ def main():
 
     st.json(asdict(dataset._issues[0]))
 
-    st.write(
+    st.markdown(
         """
     We can see that each issue may correspond to one or mode IDE versions.
     Also, the issue is described by its summary and extensive description.
@@ -64,7 +96,7 @@ def main():
     A simple text model would struggle with analyzing such data, therefore in this MVP, we will use only a summary.
 
     Some summary examples:
-    """.strip()
+    """
     )
 
     random_examples = [f"{i + 1}. {dataset[randint(0, len(dataset) - 1)][0]}" for i in range(N_RANDOM_EXAMPLES)]
@@ -85,20 +117,20 @@ def main():
 
     I trained model on provided data on Google Colab and store its weights, prediction and other necessary data.
     You may refer to [Jupyter Notebook](https://github.com/SpirinEgor/topic_modeling_story/blob/master/train_ctm.ipynb)
-    to study training process.
+    to study training process and download pretrained model from [S3](https://voudy-data.s3.eu-north-1.amazonaws.com/cmt_model.pkl).
 
     For now, let's focus on built topics based on issue summaries:
-    """.strip()
+    """
     )
 
-    components.html(build_lda_vis_html(), height=1000, scrolling=True)
+    components.html(lda_vis.prepared_data_to_html(vis_pd, template_type="simple"), height=900, scrolling=True)
 
-    st.write(
+    st.markdown(
         """
     As we can see, the model coped to detect patterns in incoming issues.
     For example, topic 1 clearly dedicated to a problem with using docker containers,
     and topic 66 is about remote interpreter.
-    """.strip()
+    """
     )
 
     st.header("Issue per version study.")
@@ -110,22 +142,49 @@ def main():
     it will allow understanding whether a bug is really fixed with the next version release.
 
     Let's look into issue distribution per PyCharm version:
-    """.strip()
+    """
     )
 
-    st.plotly_chart(version_counts())
+    st.pyplot(version_counts(dataset))
 
     st.markdown(
         """
     Nice to see that the last versions contain less and less issues 🤓.
 
-    Let's focus on topics in the pre-assigned versions.
-    At first, we will retrieve top-10 topics in `2020.2` and `2020.3` versions to see most often problems with these releases.
-    After that, we can check which topics were solved and which ones appear.
-    """.strip()
+    Let's focus on topics in the pre-assigned versions, i.e. `2020.2` and `2020.3`.
+    """
     )
 
-    st.write("IN PROGRESS 👨‍💻")
+    show_version("2020.2", dataset, prediction, vis_pd)
+    show_version("2020.3", dataset, prediction, vis_pd)
+
+    st.markdown(
+        """
+    We can see that with moving to the next version issues related to markdown colorscheme, long skeleton updates,
+    pep8 style reformat, and so on.
+    But also new version brings bugs with plugin updates,
+    running remote interpreter on windows, and docker compose support.
+    """
+    )
+
+    st.header("Trust of validity")
+    st.markdown(
+        """
+    In this MVP multiple assumptions were made:
+
+    1. Number of topics was selected manually through a couple of runs of the model and visual evaluation.
+    `100` may not be the best choice and need to be tuned in the future.
+
+    2. As BERT-backend for provided contextual topic modeling Distill Roberta is used.
+    This model was trained on corpora of English texts and then distilled in a smaller version.
+    Issues contents may be embedded in a close latent space since its one of many domains that the model saw during training.
+    Most likely, fine-tuning or training from the scratch model on programming texts would increase model quality.
+
+    3. Description contains much useful information that was skipped in this MVP.
+    The main reason for that is code snippets inside it.
+    We can detect it and build embeddings with code models, e.g. code2seq, this will bring total embeddings more information.
+    """
+    )
 
 
 if __name__ == "__main__":
